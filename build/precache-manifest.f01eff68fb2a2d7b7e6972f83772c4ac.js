565eadf2ce5e2332cfc592d2e935d51a282916c9
Htut <phyo.htut@windriver.com>
2020-02-18 00:58:23 -0800

    pretty much a wonky note taking app

---
+++ build/precache-manifest.f01eff68fb2a2d7b7e6972f83772c4ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b2ddb505758a76cb5f40a3a4b77db26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df8bba1d73594e8398c6",
+    "url": "/static/css/main.f735f04a.chunk.css"
+  },
+  {
+    "revision": "be2bdb77d58781715f07",
+    "url": "/static/js/2.063db97c.chunk.js"
+  },
+  {
+    "revision": "df8bba1d73594e8398c6",
+    "url": "/static/js/main.48f14fdd.chunk.js"
+  },
+  {
+    "revision": "b23c1d79bb49595adaa1",
+    "url": "/static/js/runtime-main.1a7ddbcc.js"
+  }
+]);
